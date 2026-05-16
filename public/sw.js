111fa099b65aefb3a4f223966b7fd0928f66600d
mehedihasankhan018-afk <mehedihasankhan018@gmail.com>
2026-05-16 11:04:52 +0600

    fixed pwa install

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,10 @@
 self.addEventListener("install", () => {
   console.log("Service Worker Installed");
 });
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker Activated");
+});
